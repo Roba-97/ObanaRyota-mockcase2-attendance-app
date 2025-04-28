4b691deb7ac837cf9795f2453e33d296ffb08eac
obana ryota <obanaryota0907@gmail.com>
2025-04-28 17:10:19 +0900

    create : 現在日付時刻のリアルタイム表示(js利用)

---
+++ src/public/js/show_current_date_time.js
@@ -0,0 +1,19 @@
+function showCurrentDateTime() {
+  const dayArray = [ "日", "月", "火", "水", "木", "金", "土" ] ;
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  const day = dayArray[now.getDay()];
+  const today = `${year}年${month}月${date}日(${day})`;
+
+  const hour = now.getHours();
+  const minute = String(now.getMinutes()).padStart(2, '0');
+  const time = `${hour}：${minute}`;
+
+  document.getElementById('current_date').textContent = today;
+  document.getElementById('current_time').textContent = time;
+}
+
+setInterval(showCurrentDateTime, 1000);
--- src/resources/views/attendance_register.blade.php
+++ src/resources/views/attendance_register.blade.php
@@ -16,11 +16,12 @@
 <div class="attendance">
     <div class="attendance__container">
         <p class="attendance__status">勤務外</p>
-        <p class="attendance__date">2023年6月1日(木)</p>
-        <p class="attendance__time">08:00</p>
+        <p id="current_date" class="attendance__date"></p>
+        <p id="current_time" class="attendance__time"></p>
         <form action="" class="attendance__form">
             <button class="attendance__button">出勤</button>
         </form>
     </div>
 </div>
+<script src="{{ asset('js/show_current_date_time.js') }}"></script>
 @endsection
