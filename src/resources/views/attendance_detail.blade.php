8827d6644751b47fa1057b744958b7939b4fa002
obana ryota <obanaryota0907@gmail.com>
2025-05-07 12:02:48 +0900

    fix : 修正申請画面のバリデーション表示対応

--- src/resources/views/attendance_detail.blade.php
+++ src/resources/views/attendance_detail.blade.php
@@ -17,7 +17,7 @@
     <div class="content__about">
         <h2 class="content__about-text">勤怠詳細</h2>
     </div>
-    <form class="attendance-detail__form" action="" method="post">
+    <form class="attendance-detail__form" action="/stamp_correction_request/{{ $attendance->id }}" method="post">
         @csrf
         <table class="form__table">
             <tr class="form__table-row">
@@ -39,21 +39,31 @@
                 <th class="form__table-header">出勤・退勤</th>
                 <td class="form__table-cel">
                     <div class="form__time-group">
-                        <input class="form__input" name="modified_punch_in" type="text" value="{{ \Carbon\Carbon::parse($attendance->punch_in)->format('H:i') }}">
+                        <input class="form__input" name="modified_punch_in" type="time"
+                            value="{{ old('modified_punch_in', \Carbon\Carbon::parse($attendance->punch_in)->format('H:i')) }}">
                         <span class="form__table-text form__table-text--time-separator">~</span>
-                        <input class="form__input" name="modified_punch_out" type="text" value="{{\Carbon\Carbon::parse($attendance->punch_out)->format('H:i') }}">
+                        <input class="form__input" name="modified_punch_out" type="time"
+                            value="{{ old('modified_punch_out', \Carbon\Carbon::parse($attendance->punch_out)->format('H:i')) }}">
                     </div>
+                    @if($errors->has('modified_punch_in') || $errors->has('modified_punch_out'))
+                    <p class="form__error-message">{{ $errors->has('modified_punch_in') ? $errors->first('modified_punch_in') : $errors->first('modified_punch_out') }}</p>
+                    @endif
                 </td>
             </tr>
-            @foreach ($attendance->breaks as $break)
+            @foreach ($attendance->breaks as $index => $break)
             <tr class="form__table-row">
                 <th class="form__table-header">休憩{{ $loop->first ? '' : $loop->iteration }}</th>
                 <td class="form__table-cel">
                     <div class="form__time-group">
-                        <input class="form__input" name="modified_break_in" type="text" value="{{ \Carbon\Carbon::parse($break->start_at)->format('H:i') }}">
+                        <input class="form__input" name="modified_break_in[]" type="time"
+                            value="{{ old("modified_break_in.$index", \Carbon\Carbon::parse($break->start_at)->format('H:i')) }}">
                         <span class="form__table-text form__table-text--time-separator">~</span>
-                        <input class="form__input" name="modified_break_out" type="text" value="{{ \Carbon\Carbon::parse($break->end_at)->format('H:i') }}">
+                        <input class="form__input" name="modified_break_out[]" type="time"
+                            value="{{ old("modified_break_out.$index", \Carbon\Carbon::parse($break->end_at)->format('H:i')) }}">
                     </div>
+                    @if($errors->has("modified_break_in.$index") || $errors->has("modified_break_out"))
+                    <p class="form__error-message">{{ $errors->has("modified_break_in.$index") ? $errors->first("modified_break_in.$index") : $errors->first("modified_break_out") }}</p>
+                    @endif
                 </td>
             </tr>
             @endforeach
@@ -61,16 +71,22 @@
                 <th class="form__table-header">休憩{{ $attendance->breaks->isNotEmpty() ? $attendance->breaks->count() + 1 : '' }}</th>
                 <td class="form__table-cel">
                     <div class="form__time-group">
-                        <input class="form__input" name="modified_break_in" type="text" value="">
+                        <input class="form__input" name="additional_break_in" type="time" value="{{ old('additional_break_in') }}">
                         <span class="form__table-text form__table-text--time-separator">~</span>
-                        <input class="form__input" name="modified_break_out" type="text" value="">
+                        <input class="form__input" name="additional_break_out" type="time" value="{{ old('additional_break_out') }}">
                     </div>
+                    @if($errors->has('additional_break_in') || $errors->has('additional_break_out'))
+                    <p class="form__error-message">{{ $errors->has('additional_break_in') ? $errors->first('additional_break_in') : $errors->first('additional_break_out') }}</p>
+                    @endif
                 </td>
             </tr>
             <tr class="form__table-row">
                 <th class="form__table-header">備考</th>
                 <td class="form__table-cel">
-                    <textarea class="form__textarea" name="comment" rows="3">電車遅延のため</textarea>
+                    <textarea class="form__textarea" name="comment" rows="3" placeholder="電車遅延のため">{{ old('comment') }}</textarea>
+                    @error('comment')
+                    <p class="form__error-message">{{ $message }}</p>
+                    @enderror
                 </td>
             </tr>
         </table>
