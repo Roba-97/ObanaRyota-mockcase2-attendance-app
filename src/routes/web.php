700ca0336ef47103cb460df433ae7ca1f5043d06
obana ryota <obanaryota0907@gmail.com>
2025-05-22 18:07:05 +0900

    create : メール認証ルーティングの作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\StampController;
 use App\Http\Controllers\ModificationController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\LoginController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::prefix('attendance')->group(function() {
         Route::get('/', [StampController::class, 'index']);
         Route::post('/punch_in', [StampController::class, 'createStamp']);
@@ -42,5 +44,21 @@ Route::prefix('admin')->group(function () {
     Route::get('/logout', [LoginController::class, 'adminLogout']);
 });
 
+// メール確認の通知
+Route::get('/email/verify', function () {
+    return view('auth.verify_email');
+})->middleware('auth')->name('verification.notice');
 
+// メール確認のハンドラ
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
 
+    return redirect('/attendance');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+// メール確認の再送信
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', '認証メールを再送しました。');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
