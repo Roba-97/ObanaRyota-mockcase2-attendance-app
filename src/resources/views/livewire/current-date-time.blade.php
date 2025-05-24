dfe41e50ead430e96b691f03ac21a851486003b9
obana ryota <obanaryota0907@gmail.com>
2025-05-24 16:35:24 +0900

    create : 勤怠登録画面の表示テストの初版作成

--- src/resources/views/livewire/current-date-time.blade.php
+++ src/resources/views/livewire/current-date-time.blade.php
@@ -3,7 +3,7 @@
     $weekDays = ['日', '月', '火', '水', '木', '金', '土'];
     @endphp
     <p class="attendance__date">
-        {{ $now->year }}年{{ $now->month }}月{{ $now->day }}({{ $weekDays[$now->dayOfWeek] }})
+        {{ $now->year }}年{{ $now->month }}月{{ $now->day }}日({{ $weekDays[$now->dayOfWeek] }})
     </p>
     <p class="attendance__time">
         {{ $now->format('H:i') }}
